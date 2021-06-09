938d26de0ffb4c92cc359c6ea842b5afea550006
srdante <minidantebtc@gmail.com>
2021-06-09 01:32:14 -0300

    0.2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const tailwindBase = plugin(function ({ addBase, theme }) {
   addBase({
     // h* in common
     'h1, h2, h3, h4, h5, h6': { color: theme('colors.gray.900'), leading: theme('leading.6') },
-    '.dark h1, .dark h2, .dark h3, .dark h4, .dark h5, .dark h6': { color: theme('colors.gray.200') },
+    '.dark h1, .dark h2, .dark h3, .dark h4, .dark h5, .dark h6': { color: theme('colors.white') },
+
     // h*
     'h1': { fontSize: theme('fontSize.3xl'), fontWeight: theme('fontWeight.semibold') },
     'h2': { fontSize: theme('fontSize.2xl'), fontWeight: theme('fontWeight.semibold') },
@@ -12,16 +13,19 @@ const tailwindBase = plugin(function ({ addBase, theme }) {
     'h4': { fontSize: theme('fontSize.lg'), fontWeight: theme('fontWeight.medium') },
     'h5': { fontSize: theme('fontSize.base'), fontWeight: theme('fontWeight.medium') },
     'h6': { fontSize: theme('fontSize.sm'), fontWeight: theme('fontWeight.medium') },
-    // p
-    'p': { fontSize: theme('fontSize.sm'), color: theme('colors.gray.500') },
-    // dark p
-    '.dark p': { color: theme('colors.gray.300') },
-    // dark theme border
-    '.dark *, .dark ::before, .dark ::after': { borderColor: theme('colors.gray.600', 'currentColor') },
+
+    // p, li
+    'p, li': { fontSize: theme('fontSize.sm'), color: theme('colors.gray.500') },
+    // dark p, dark li
+    '.dark p, .dark li': { color: theme('colors.gray.300') },
+
     // body
     'body': { backgroundColor: theme('colors.gray.100') },
     // dark body
-    '.dark body': { backgroundColor: theme('colors.gray.700') }
+    '.dark body': { backgroundColor: theme('colors.gray.700') },
+
+    // dark theme border
+    '.dark *, .dark ::before, .dark ::after': { borderColor: theme('colors.gray.600', 'currentColor') },
   })
 })
 
